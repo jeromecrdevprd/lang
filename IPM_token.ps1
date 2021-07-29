fb4c9043935692aa49620dfc1259fd74282945fa
jeromecrdevprd <jerome.cruz@gatesfoundation.org>
2021-07-29 03:22:03 -0700

    add

---
+++ IPM_token.ps1
@@ -0,0 +1,12 @@
+﻿
+$resourceAppIdURI = 'https://database.windows.net/'
+
+$tokenResponse = Invoke-RestMethod -Method Post -UseBasicParsing -Uri "https://login.windows.net/$($args[0])/oauth2/token" -Body @{resource=$resourceAppIdURI 
+client_id=$args[1] 
+grant_type='client_credentials' 
+client_secret=$args[2]} -ContentType 'application/x-www-form-urlencoded'
+
+if ($tokenResponse) {
+Write-debug “Access token type is $($tokenResponse.token_type), expires $($tokenResponse.expires_on)”
+$Token = $tokenResponse.access_token | Out-file .env
+}
